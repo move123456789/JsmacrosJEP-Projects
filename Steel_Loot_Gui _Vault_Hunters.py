8e2173bba871909d12738e1051060dbf52b08651
SmokyAce <julian.hansen2002@gmail.com>
2023-01-15 00:06:58 +0100

    Steel Loot w/GUI V1.0 Vault Hunters

--- Steel_Loot_Gui _Vault_Hunters.py
+++ Steel_Loot_Gui _Vault_Hunters.py
@@ -1,7 +1,7 @@
 # More Info Soon
 # This is steel_loot combined with whitelist
 # You can create white_list with items_to_list.py
-# V0.3 14.01.23 22:32
+# V1.0 15.01.23 00:07
 
 if __name__ == "":
     from JsMacrosAC import *
@@ -13,9 +13,6 @@ debug = True
 # == DEBUG ==
 
 
-# white_lst = ['the_vault:faceted_focus', 'the_vault:fundamental_focus', 'the_vault:resilient_focus',
-#              'the_vault:opportunistic_focus', 'the_vault:nullifying_focus', 'the_vault:amplifying_focus',
-#              'the_vault:wild_focus']
 white_lst = ['the_vault:silver_scrap', 'the_vault:gorgeous_jewel', 'the_vault:ash_jewel', 'the_vault:benite_jewel', 'the_vault:bombing_jewel', 'the_vault:echoing_jewel', 'the_vault:petzan_jewel', 'the_vault:pogging_jewel', 'the_vault:tubic_jewel', 'the_vault:xen_jewel', 'the_vault:sparking_jewel', 'the_vault:iskallic_jewel', 'the_vault:upal_jewel', 'the_vault:phoenix_dust', 'the_vault:phoenix_feather', 'the_vault:dreamstone', 'the_vault:eye_of_avarice', 'the_vault:gem_benitoite', 'the_vault:gem_larimar', 'the_vault:gem_black_opal', 'the_vault:gem_painite', 'the_vault:gem_iskallium', 'the_vault:gem_gorginite', 'the_vault:gem_sparkletine', 'the_vault:gem_ashium', 'the_vault:gem_bomignite', 'the_vault:gem_tubium', 'the_vault:gem_wutodie', 'the_vault:gem_upaline', 'the_vault:gem_puffium', 'the_vault:gem_petzanite', 'the_vault:gem_xenium', 'the_vault:gem_echo', 'the_vault:vault_rock', 'the_vault:gem_pog', 'the_vault:relic_booster_pack', 'the_vault:skill_shard', 'the_vault:bitter_lemon', 'the_vault:sour_orange', 'the_vault:mystic_pear', 'the_vault:key_piece', 'the_vault:poisonous_mushroom', 'the_vault:vault_diamond', 'the_vault:skill_essence', 'the_vault:mote_clarity', 'the_vault:mote_purity', 'the_vault:mote_sanctity', 'the_vault:burger_cheese', 'the_vault:burger_lettuce', 'the_vault:burger_tomato', 'the_vault:vault_cookie', 'the_vault:skill_orb', 'the_vault:gem_alexandrite', 'the_vault:mystery_egg', 'the_vault:acceleration_chip', 'the_vault:pandoras_box', 'the_vault:eternal_soul', 'the_vault:knowledge_star_shard', 'the_vault:knowledge_star_essence', 'the_vault:knowledge_star', 'the_vault:sword', 'the_vault:axe', 'the_vault:idol_timekeeper', 'the_vault:idol_omniscient', 'the_vault:idol_benevolent', 'the_vault:etching', 'the_vault:shield', 'the_vault:boots', 'the_vault:leggings', 'the_vault:chestplate', 'the_vault:helmet', 'the_vault:idol_malevolence', 'the_vault:unidentified_relic_fragment', 'the_vault:sweet_kiwi', 'the_vault:hunter_eye', 'the_vault:burger_patty', 'the_vault:burger_bun', 'the_vault:vault_scrap', 'the_vault:vault_ingot', 'the_vault:mystery_box', 'the_vault:empty_flask', 'the_vault:wild_focus', 'the_vault:amplifying_focus', 'the_vault:nullifying_focus', 'the_vault:vault_alloy', 'the_vault:fundamental_focus', 'the_vault:rune', 'the_vault:vault_catalyst_fragment', 'the_vault:soul_flame', 'the_vault:faceted_focus', 'the_vault:crystal_seal_empty', 'the_vault:perfect_echo_gem', 'the_vault:resilient_focus', 'the_vault:vault_plating', 'the_vault:vault_essence', 'the_vault:magnetite', 'the_vault:vault_nugget', 'the_vault:vault_dust', 'the_vault:hearty_apple', 'the_vault:vault_apple', 'the_vault:raw_chromatic_iron', 'the_vault:chromatic_iron_nugget', 'the_vault:chromatic_iron_ingot', 'the_vault:chromatic_steel_nugget', 'the_vault:black_chromatic_steel_ingot', 'the_vault:magic_silk', 'the_vault:chipped_vault_rock', 'the_vault:carbon', 'the_vault:carbon_nugget', 'the_vault:vault_meat', 'the_vault:treasure_chest_scroll', 'the_vault:altar_chest_scroll', 'the_vault:living_chest_scroll', 'the_vault:gilded_chest_scroll', 'the_vault:ornate_chest_scroll', 'the_vault:wooden_chest_scroll', 'the_vault:driftwood', 'the_vault:diamond_nugget', 'the_vault:cooked_vault_steak', 'the_vault:extraordinary_benitoite', 'the_vault:extraordinary_larimar', 'the_vault:extraordinary_wutodie', 'the_vault:regret_nugget', 'the_vault:regret_chunk', 'the_vault:regret_orb', 'the_vault:aura_scroll', 'the_vault:unidentified_treasure_key', 'the_vault:mod_box', 'the_vault:memory_shard', 'the_vault:trinket']
 
 lst = len(white_lst)
@@ -118,6 +115,7 @@ def load_lst(a9, a10):
             Chat.log(saved_list)
     except FileNotFoundError:
         Chat.log("Filed Doesnt Exist")
+
 
 def add_all_to_lst(a12, a13):
     global active_lst
