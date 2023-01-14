0d3b4152e099d59e8c340e3b75924ae3815336c4
SmokyAce <julian.hansen2002@gmail.com>
2023-01-14 15:49:37 +0100

    Changes

---
+++ README.txt
@@ -0,0 +1,4 @@
+To use the JsMacros-Jep Scripts you need to install JEP and JsMacrosJEP (Intstallation tutorial in dev).
+
+After installation the scripts should work.
+Explanation on how to use the scripts i made are inside the files
--- whitelist.py
+++ whitelist.py
@@ -2,13 +2,14 @@ if __name__ == "":
     from JsMacrosAC import *
 import ast
 
-white_lst = ['the_vault:faceted_focus', 'the_vault:fundamental_focus', 'the_vault:resilient_focus', 'the_vault:opportunistic_focus', 'the_vault:nullifying_focus', 'the_vault:amplifying_focus', 'the_vault:wild_focus']
+white_lst = ['the_vault:faceted_focus', 'the_vault:fundamental_focus', 'the_vault:resilient_focus',
+             'the_vault:opportunistic_focus', 'the_vault:nullifying_focus', 'the_vault:amplifying_focus',
+             'the_vault:wild_focus']
 lst = len(white_lst)
 click_lst = []
 active_lst = []
 btn_clicked_one = 0
 btn_clicked_two = 0
-
 
 screen = Hud.createScreen("Test", False)
 
@@ -82,13 +83,15 @@ def save_lst(a7, a8):
 
 
 def load_lst(a9, a10):
-    with open("C:/Users/Julian/PycharmProjects/Minecraft jsMacros/Linked/config_whitelist.txt") as f:
-        saved_list_str = f.read()
-        f.close()
-        # saved_list = list(saved_list_str.split())
-        saved_list = ast.literal_eval(saved_list_str)
-    Chat.log("Loaded Whitelist")
-    Chat.log(saved_list)
+    try:
+        with open("C:/Users/Julian/PycharmProjects/Minecraft jsMacros/Linked/config_whitelist.txt") as f:
+            saved_list_str = f.read()
+            f.close()
+            saved_list = ast.literal_eval(saved_list_str)
+        Chat.log("Loaded Whitelist")
+        Chat.log(saved_list)
+    except FileNotFoundError:
+        Chat.log("Filed Doesnt Exist")
 
 
 def screen_init(screen):
@@ -105,7 +108,8 @@ def screen_init(screen):
     screen.addButton(x_close, y_close, 100, 20, "Close", JavaWrapper.methodToJava(close))
     screen.addItem(100, 100, "minecraft:oak_planks", screen)
     screen.addButton(320, 20, 15, 15, "", JavaWrapper.methodToJava(callback_two))
-    screen.addButton(320, 40, 15, 15, "", JavaWrapper.methodToJava(lambda btn, ctx, bt=0: btn.setLabel("X") if bt==0 else btn.setLabel("")))
+    screen.addButton(320, 40, 15, 15, "", JavaWrapper.methodToJava(
+        lambda btn, ctx, bt=0: btn.setLabel("X") if bt == 0 else btn.setLabel("")))
     screen.addButton(340, 40, 15, 15, "", JavaWrapper.methodToJava(
         lambda btn, ctx, bt=1, str="minecraft:diamond": btn.setLabel("X") and print_str(str)))
     screen.addButton(360, 40, 15, 15, "", JavaWrapper.methodToJava(
@@ -114,7 +118,8 @@ def screen_init(screen):
     for index, value in enumerate(white_lst):
         index = 20 + index * 20
         screen.addItem(140, index, value, screen)
-        screen.addButton(160, index, 10, 10, "", JavaWrapper.methodToJava(lambda btn, ctx, str=value: btn.setLabel("X") and print_str(str)))
+        screen.addButton(160, index, 10, 10, "",
+                         JavaWrapper.methodToJava(lambda btn, ctx, str=value: btn.setLabel("X") and print_str(str)))
 
 
 def rm(ctx):
@@ -123,4 +128,3 @@ def rm(ctx):
 
 
 Chat.createCommandBuilder('ov').executes(JavaWrapper.methodToJavaAsync(rm)).register()
-
